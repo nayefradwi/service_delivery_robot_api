5db6ad02c7350b0840c1b089041b14d799a01351
naifr <nr1707337@qu.edu.qa>
2020-11-04 02:55:32 +0300

    modified necessity model and hospital model

--- api/models/Hospital.js
+++ api/models/Hospital.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 hospitalSchema = new mongoose.Schema({
     hospitalName: String,
+    logoUrl:String,
 });
 
 module.exports = mongoose.model("Hospital", hospitalSchema);
--- api/models/Necessity.js
+++ api/models/Necessity.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 necessitySchema = new mongoose.Schema({
     name: String,
+    iconUrl: String,
 });
 
 module.exports = mongoose.model("Necessity", necessitySchema);
