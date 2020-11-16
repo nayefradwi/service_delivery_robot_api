62c5fe292161da9811ba5aaf462fe8853ebe48a9
naifr <nr1707337@qu.edu.qa>
2020-11-16 23:10:39 +0300

    deploying server part 1;
    fifth attempt to deploying to heroku

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cp = require('cookie-parser');
 const parser = require("body-parser");
@@ -5,7 +6,6 @@ const app = express();
 const socketService = require("./api/services/SocketIoService");
 const io = socketService(app)
 const mongoose = require("mongoose");
-require("dotenv").config();
 const DATABASE_URL =  process.env.DB_URL||"mongodb://localhost/senior"
 const PORT = process.env.PORT || 6969
 const indexRouter = require("./api/routes/index");
