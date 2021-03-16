51b802882a892a3598466bc26697fea3d9ea3e8a
naifr <nr1707337@qu.edu.qa>
2021-03-16 14:07:39 +0300

    finished get tasks for nurse

--- api/repos/NurseRepo.js
+++ api/repos/NurseRepo.js
@@ -1,90 +1,88 @@
-const Nurse = require("../models/Nurse")
+const Nurse = require("../models/Nurse");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const secret = process.env.SECRET || "this is a secret";
 const mongoose = require("mongoose");
 
 class NurseRepo {
+  async getNurseFromNurseId(nurseId) {
+    return Nurse.findOne({ nurseId: nurseId });
+  }
 
-    async getNurseFromNurseId(nurseId) {
-        return Nurse.findOne({nurseId: nurseId});
+  async login(nurseLoggingIn) {
+    const nurse = await this.getNurseFromNurseId(nurseLoggingIn.nurseId);
+    if (!nurse) return "nurse could not be found";
+    // if (nurse.isLoggedIn)
+    //     return "this user is already logged in";
+    const isMatch = await bcrypt.compare(
+      nurseLoggingIn.password,
+      nurse.password
+    );
+    if (isMatch) {
+      nurse.fcmTokens = nurseLoggingIn.fcmToken;
+      nurse.isLoggedIn = true;
+      await nurse.save();
+      const payload = {
+        _id: nurse._id,
+        isRoom: false,
+        isNurse: true,
+        isAdmin: false,
+      };
+      return { jwt: await jwt.sign(payload, secret) };
+    } else {
+      return "password is incorrect";
     }
+  }
 
-    async login(nurseLoggingIn) {
-        const nurse = await this.getNurseFromNurseId(nurseLoggingIn.nurseId);
-        if (!nurse)
-            return "nurse could not be found";
-        if (nurse.isLoggedIn)
-            return "this user is already logged in";
-        const isMatch = await bcrypt.compare(nurseLoggingIn.password, nurse.password,)
-        if (isMatch) {
-            nurse.fcmTokens = nurseLoggingIn.fcmToken;
-            nurse.isLoggedIn = true;
-            await nurse.save();
-            const payload = {
-                _id: nurse._id,
-                isRoom: false,
-                isNurse: true,
-                isAdmin: false,
-            };
-            return {jwt: await jwt.sign(payload, secret,)};
-        } else {
-            return "password is incorrect";
-        }
-    }
-
-    async register(nurse) {
-        if (!nurse.password)
-            return "please fill in a password";
-        const nurseFound = await this.getNurseFromNurseId(nurse.nurseId);
-        if (nurseFound)
-            return "nurse id already exists";
-        const salt = await bcrypt.genSalt(10);
-        if (salt) {
-            const hash = await bcrypt.hash(nurse.password, salt);
-            if (hash) {
-                const admin = await Nurse.create({
-                    nurseId: nurse.nurseId,
-                    hospital: nurse.hospital,
-                    password: hash,
-                });
-                if (!admin)
-                    return "failed to register";
-                return true;
-            }
-        }
-        return "unknown error";
-    }
-
-    async edit(nurse, newNurse) {
-        if (newNurse.nurseId.toString() !== nurse.nurseId.toString()) {
-            const otherNurse = await this.getNurseFromNurseId(newNurse.nurseId)
-            if (otherNurse)
-                return "nurse id already exists"
-        }
-        nurse.nurseId = newNurse.nurseId;
-        nurse.hospital = newNurse.hospital;
-        const edited = await nurse.save();
-        if (!edited)
-            return "failed to edit nurse";
+  async register(nurse) {
+    if (!nurse.password) return "please fill in a password";
+    const nurseFound = await this.getNurseFromNurseId(nurse.nurseId);
+    if (nurseFound) return "nurse id already exists";
+    const salt = await bcrypt.genSalt(10);
+    if (salt) {
+      const hash = await bcrypt.hash(nurse.password, salt);
+      if (hash) {
+        const admin = await Nurse.create({
+          nurseId: nurse.nurseId,
+          hospital: nurse.hospital,
+          password: hash,
+        });
+        if (!admin) return "failed to register";
         return true;
+      }
     }
+    return "unknown error";
+  }
 
-    async deleteAllNursesByHospitalId(hospitalId) {
-        return Nurse.delete({hospital: hospitalId});
+  async edit(nurse, newNurse) {
+    if (newNurse.nurseId.toString() !== nurse.nurseId.toString()) {
+      const otherNurse = await this.getNurseFromNurseId(newNurse.nurseId);
+      if (otherNurse) return "nurse id already exists";
     }
+    nurse.nurseId = newNurse.nurseId;
+    nurse.hospital = newNurse.hospital;
+    const edited = await nurse.save();
+    if (!edited) return "failed to edit nurse";
+    return true;
+  }
 
-    async getNurseByObjectId(id) {
-        return Nurse.findOne({_id: id}, {password: 0}).populate("hospital")
-    }
+  async deleteAllNursesByHospitalId(hospitalId) {
+    return Nurse.delete({ hospital: hospitalId });
+  }
 
-    async getNursesByHospitalId(hospitalId, skip, limit){
-        return Nurse.find({hospital:hospitalId}, {password:0, hospital:0}).skip(parseInt(skip)).limit(parseInt(limit));
-    }
+  async getNurseByObjectId(id) {
+    return Nurse.findOne({ _id: id }, { password: 0 }).populate("hospital");
+  }
 
-    async delete(id){
-        return Nurse.deleteOne({_id:id});
-    }
+  async getNursesByHospitalId(hospitalId, skip, limit) {
+    return Nurse.find({ hospital: hospitalId }, { password: 0, hospital: 0 })
+      .skip(parseInt(skip))
+      .limit(parseInt(limit));
+  }
+
+  async delete(id) {
+    return Nurse.deleteOne({ _id: id });
+  }
 }
 
 module.exports = new NurseRepo();
--- app.js
+++ app.js
@@ -1,44 +1,46 @@
 require("dotenv").config();
 const express = require("express");
-const cp = require('cookie-parser');
+const cp = require("cookie-parser");
 const parser = require("body-parser");
 const app = express();
 const socketService = require("./api/services/SocketIoService");
-const io = socketService(app)
+const io = socketService(app);
 const mongoose = require("mongoose");
-const DATABASE_URL =  process.env.DB_URL||"mongodb://localhost/senior"
-const PORT = process.env.PORT || 6969
+const DATABASE_URL = process.env.DB_URL || "mongodb://localhost/senior";
+const PORT = process.env.PORT || 6969;
 const indexRouter = require("./api/routes/index");
 const hospitalRouter = require("./api/routes/hospital");
 const nurseRouter = require("./api/routes/nurse");
-const necessityRouter = require("./api/routes/necessity")
-const roomRouter = require("./api/routes/room")
-const mapRouter = require("./api/routes/map")
-const taskRouter = require("./api/routes/task")
-const notificationService = require("./api/services/NotificationService")
+const necessityRouter = require("./api/routes/necessity");
+const roomRouter = require("./api/routes/room");
+const mapRouter = require("./api/routes/map");
+const taskRouter = require("./api/routes/task");
+const notificationService = require("./api/services/NotificationService");
 app.use(parser.json());
 app.use(cp());
 notificationService.initialize();
 
-const {createAdmin} = require("./api/seed")
+const { createAdmin } = require("./api/seed");
 
 mongoose.connect(DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
-app.get("/",(req,res)=>res.send("hello world"))
-app.get("/index",(req,res)=>res.send("hello"))
+app.get("/", (req, res) => res.send("hello world"));
+app.get("/index", (req, res) => res.send("hello"));
 
 // createAdmin();
 app.use(indexRouter);
-app.use("/hospitals",hospitalRouter);
-app.use("/nurses",nurseRouter);
-app.use("/necessities", necessityRouter)
-app.use("/rooms", roomRouter)
-app.use("/maps", mapRouter)
-app.use("/tasks",taskRouter);
+app.use("/hospitals", hospitalRouter);
+app.use("/nurses", nurseRouter);
+app.use("/necessities", necessityRouter);
+app.use("/rooms", roomRouter);
+app.use("/maps", mapRouter);
+app.use("/tasks", taskRouter);
 
-io.server.listen(PORT, () => console.log("server started on port: "+ PORT.toString()))
+io.server.listen(PORT, () =>
+  console.log("server started on port: " + PORT.toString())
+);
